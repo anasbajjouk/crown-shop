07c4a3baa5f1668a4853ec0ffc26bdf3538c7256
Anas Bajjouk <bajjouk1@gmail.com>
2019-07-29 11:14:57 +0200

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -18,10 +18,18 @@ export default class SignIn extends Component {
         }
     }
     
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email:'', password:'' })
+        const { email, password } = this.state;
+
+        try {
+            await auth.createUserWithEmailAndPassword(email, password);
+            this.setState({ email:'', password:'' })
+        } catch (error) {
+            console.log(error);
+        }
+        
     }
 
     handleChange = event => {
